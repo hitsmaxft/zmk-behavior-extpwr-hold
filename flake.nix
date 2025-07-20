3c0ec7ffc5dcf484fde3e572a38086d93594e9e1
hitsmaxft <mfthits@gmail.com>
2025-07-20 10:27:15 +0800

    add test

--- flake.nix
+++ flake.nix
@@ -31,7 +31,6 @@
           pkgs = nixpkgs.legacyPackages.${system};
           zephyr = zephyr-nix.packages.${system};
           zephyrPyEnv = zephyr-nix.packages.${system}.pythonEnv;
-          keymap_drawer = keymap_drawer-nix.packages.${system}.default;
         in {
           default = pkgs.mkShellNoCC {
             packages = with pkgs; [
@@ -55,6 +54,27 @@
             ];
 
             shellHook = ''
+
+ZEPHYR_BASE=$(west config 'zephyr.base')
+
+if [[ -n $ZEPHYR_BASE ]] ; then
+    echo "found ZEPHYR_BASE: $ZEPHYR_BASE"
+
+    Zephyr_DIR=$ZEPHYR_BASE/share/zephyr-package/cmake/
+
+    LIB_BASE_DIR="$(dirname "$ZEPHYR_BASE")"
+
+    ZMK_SRC_DIR={'$'}{LIB_BASE_DIR}/zmk/app
+
+    export LIB_BASE_DIR
+    export ZMK_SRC_DIR
+    export Zephyr_DIR
+else
+    echo "found ZEPHYR_BASE not found: $ZEPHYR_BASE"
+    echo "please run just update-config"
+fi
+
+
             '';
           };
         });
